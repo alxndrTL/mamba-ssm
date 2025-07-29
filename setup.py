eed97c8a89615510a5ce2613f414ceff576e342c
Alexandre TL <alextorresleguet@icloud.com>
2025-07-29 15:50:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -185,12 +185,12 @@ if not SKIP_CUDA_BUILD:
         cc_flag.append("-gencode")
         cc_flag.append("arch=compute_87,code=sm_87")
 
-        if bare_metal_version >= Version("11.8"):
-            cc_flag.append("-gencode")
-            cc_flag.append("arch=compute_90,code=sm_90")
-        if bare_metal_version >= Version("12.8"):
-            cc_flag.append("-gencode")
-            cc_flag.append("arch=compute_100,code=sm_100")
+        #if bare_metal_version >= Version("11.8"):
+        cc_flag.append("-gencode")
+        cc_flag.append("arch=compute_90,code=sm_90")
+        #if bare_metal_version >= Version("12.8"):
+        cc_flag.append("-gencode")
+        cc_flag.append("arch=compute_100,code=sm_100")
 
 
     # HACK: The compiler flag -D_GLIBCXX_USE_CXX11_ABI is set to be the same as
